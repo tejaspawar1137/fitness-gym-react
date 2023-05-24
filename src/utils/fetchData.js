de2e9f148689865333eb04947edf0e67545c5a69
tejas <tejaspawar183@gmail>
2023-05-24 13:46:38 +0530

    fitness gym

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,22 +1,21 @@
 export const exerciseOptions = {
   method: 'GET',
   headers: {
+    'X-RapidAPI-Key': '35d99de19dmsh88f392fcce20d3ap1f86c2jsnfd99a3369fc7',
     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
   },
 };
 
 export const youtubeOptions = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
-    'X-RapidAPI-Key': 'f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85',
+    'X-RapidAPI-Key': '35d99de19dmsh88f392fcce20d3ap1f86c2jsnfd99a3369fc7',
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
   },
 };
 
 export const fetchData = async (url, options) => {
   const res = await fetch(url, options);
   const data = await res.json();
-
   return data;
 };
